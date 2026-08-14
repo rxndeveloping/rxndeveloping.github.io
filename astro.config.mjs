4a2d15f85a0a10ed92e7bb7fd0990d059a0aaece
Ryze <businessrxndeveloping@gmail.com>
2026-08-14 15:30:11 +0200

    Fix square team avatars and page style loading

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ export default defineConfig({
   output: 'static',
   trailingSlash: 'always',
   build: {
-    format: 'directory'
+    format: 'directory',
+    inlineStylesheets: 'always'
   }
 });
